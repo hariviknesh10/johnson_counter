c946d29c0f4ef1629d5bcbeafd77f7e26cfa396b
hariviknesh10 <164751001+hariviknesh10@users.noreply.github.com>
2025-01-02 14:35:11 +0530

    Update 5bit_john_counter_tb.v

--- 5bit_john_counter_tb.v
+++ 5bit_john_counter_tb.v
@@ -1,1 +1,15 @@
-
+module johnson_tb;
+reg clk, rst;
+wire [4:0] count;
+johnson_rtl uut (
+    .clk(clk),
+    .rst(rst),
+    .count(count));
+initial begin
+    clk = 0;
+    rst = 1;
+    #10 
+    rst = 0;
+end
+always #5 clk = ~clk;
+endmodule
